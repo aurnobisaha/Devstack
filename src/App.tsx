d5733f11ad1b96833d08902f71e3a0806371e506
Aurnobi Saha <aurnobi.edu@gmail.com>
2026-09-13 12:59:16 +0600

    Add heading

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,12 @@ const cardspromise=cardsfetch();
    
    <Nav />
    <Banner />
+   <div className='px-25'>
+   <h1 className='text-4xl font-bold text-[#0F172A]'>
+    Explore the <span className='bg-gradient-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent'>Technologies</span>
+   </h1>
+   <p className='text-[#64748B]'>Pick one technology per category to build your ideal stack.</p>
+   </div>
    <Suspense fallback={<h2>Loading.....</h2>}> 
      <Cards cardspromise={cardspromise} />
    </Suspense>
